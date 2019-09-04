fa167d81aaacaab80c79f5ac6b18c1d24c3810e2
Ryanho <ryanho2013@163.com>
2019-09-04 18:12:27 +0800

    add RegressionTree in CART.py

--- DA_Algorithm/CART.py
+++ DA_Algorithm/CART.py
@@ -1,5 +1,6 @@
 #encoding = utf-8
 
+#分类树
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
@@ -7,6 +8,7 @@ from sklearn.datasets import load_iris
 
 #准备数据集
 iris = load_iris()
+
 #获取特征集和分类标识
 features = iris.data
 labels = iris.target
@@ -22,3 +24,33 @@ test_predict = clf.predict(test_features)
 score = accuracy_score(test_labels, test_predict)
 
 print("the score is %.4lf" % score)
+
+
+#回归树
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_boston
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
+
+#准备数据
+boston = load_boston()
+#探索数据
+print(boston.feature_names)
+#获取特征集和房价
+features = boston.data
+price = boston.target
+#随机抽取33%的数据作为测试集，其余的为训练集
+train_features, test_features, train_price, test_price = train_test_split(features, price, test_size = 0.33)
+#创建回归树
+dtr = DecisionTreeRegressor()
+#拟合构造CART回归树
+dtr.fit(train_features, train_price)
+#预测测试集中的房价
+predict_price = dtr.predict(test_features)
+
+#测试集的评价结果
+print("mean_squared_error : ", mean_squared_error(test_price, predict_price))
+print("mean_absolute_error : ", mean_absolute_error(test_price, predict_price))
+
+
